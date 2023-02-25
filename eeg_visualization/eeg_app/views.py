0f624b00717229559a31f5eaea408fea9b61a354
jmholmquist <jmholmquist@crimson.ua.edu>
2023-02-24 23:00:16 -0600

    Added post handler in views

--- eeg_visualization/eeg_app/views.py
+++ eeg_visualization/eeg_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def handlePost(request):
+    print("print working")
+    print(request.body)
+    print("after second print")
+    
+    x = {
+    "sensor1": [1, 5, 7],
+    "sensor2": [2, 3, 6],
+    "sensor3": [5, 3, 1]
+    }
+    y = json.dumps(x)
+    return HttpResponse(y)
