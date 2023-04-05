5e23b7b74db9855b539d7e3144e478b5fb4fcfab
jmholmquist <jmholmquist@crimson.ua.edu>
2023-04-04 23:10:43 -0500

    added configuration files and a bash script to set up server

---
+++ eeg_visualization/launch_server.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install ffmpeg libsm6 libxext6  -y
+sudo apt-get install python3.10 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install gunicorn -y
+sudo apt-get install nginx -y
+
+sudo python3 -m pip install --no-cache-dir --upgrade pip
+sudo python3 -m pip install --no-cache-dir --upgrade open3d
+sudo python3 -m pip install --no-cache-dir --upgrade Django
+sudo python3 -m pip install --no-cache-dir --upgrade scikit-learn
+
+sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
+sudo cp gunicorn.socket /etc/systemd/system/gunicorn.socket
+sudo cp site.conf /etc/nginx/sites-available/eeg_visualization
+sudo cp nginx.conf /etc/nginx/nginx.conf
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic
+
+sudo systemctl start nginx
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
+sudo ln -s /etc/nginx/sites-available/eeg_visualization /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full'
+
+
+
